f166656b445d8523ef156e36ca153d3e2d1ee531
yagmurbrnk <yagmurbarankarakus@outlook.com>
2023-10-01 13:26:10 +0300

    updated slider
    changed icons  and add logo

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
+
 const Slider = () => {
     const images = [
-        "https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
-        "https://images.unsplash.com/photo-1619722087489-f0b1a6fdbc6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+        "https://media.discordapp.net/attachments/804063971405070356/1157960303544377405/IMG_5490.jpg?ex=651a8212&is=65193092&hm=ee17df27a10e43b183c1ebd7af3ce9e6fdb028e5932b4b17d95b1438e30ac432&=&width=1440&height=401",
+        "https://media.discordapp.net/attachments/804063971405070356/1157963146879836240/IMG_5493.jpg?ex=651a84b8&is=65193338&hm=2a92d0d73650c334c17fcea9217ac361d7e920f6d9c01adc60a3582d5c8170b2&=&width=1440&height=401",
+        "https://media.discordapp.net/attachments/804063971405070356/1157965646630486096/IMG_5494.jpg?ex=651a870c&is=6519358c&hm=fb3c634c70f16484b4ec274f20eb717de87b36a5ac095b3f3d05c993e4ab8b14&=&width=1440&height=397",
+
     ];
 
     return (
         <Slide>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${images[0]})` }}>
-                    <span className="border-gray-50 rounded">Welcome to SHIRE</span>
+             
                 </div>
             </div>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${images[1]})` }}>
-                    <span className="border-gray-50 rounded">Welcome to SHIRE</span>
+                  
+                </div>
+
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[2]})` }}>
+            
                 </div>
             </div>
         </Slide>
     )
 };
-
 
 export default Slider;
