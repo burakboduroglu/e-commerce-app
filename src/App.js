2c10a71a489ec4da892457c28b6119f7d4a6f84e
Derya Nur <nurderya791@gmail.com>
2023-10-02 22:39:25 +0300

    search bar

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import './css/App.css';
-import Products from './components/Products';
-import { useState, useEffect } from 'react';
-import takeData from './data.js' ;
+import "./css/App.css";
+import Products from "./components/Products";
+import { useState, useEffect } from "react";
+import takeData from "./data.js";
 import Footer from "./components/Footer";
-import './css/Slider.css'
+import "./css/Slider.css";
 import Slider from "./components/Slider";
 import search from "./search";
-import {NavbarWithMegaMenu} from "./components/Header";
+import { NavbarWithMegaMenu } from "./components/Header";
 import Category from "./components/Category";
 import SearchResults from "./components/SearchResults";
 
 function App() {
   const [products, setProducts] = useState([]);
-  useEffect(() => {
-    takeData('https://dummyjson.com/products/').then((data) => setProducts(data));
-  }, []);
 
-  
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    takeData(query).then((data) => setProducts(data));
+  }, [query]);
+
   const handleSubmit = (query) => {
     console.log(query);
-    takeData(query).then((data) => setProducts(data))
-  }
+    takeData(query).then((data) => setProducts(data));
+  };
 
   return (
     <div className="container m-auto mt-3 tile col-span-3 md:col-span-5 lg:col-span-8">
-        <div className='sticky top-3 z-10'>
-            <NavbarWithMegaMenu search={handleSubmit}/>
-        </div>
-        <div className="mt-3">
-            <Slider />
-        </div>
-        <div className="mt-3 flex items-center justify-center">
-            <h3 className="uppercase font-sans text-xl font-bold">
-                Products
-            </h3>
-        </div>
-        <div className="mt-3 flex items-center justify-center">
-            <h4 className="bg-red-50 rounded">Category:
-                <span className="pl-1 capitalize font-sans font-sm font-bold text-cyan-800">
-                    {
-                        (products?.[1]?.category === products?.[products.length-1]?.category) 
-                        ? 
-                        products?.[1]?.category 
-                        :
-                        ("General")
-                    }
-                </span>
-            </h4>
-        </div>
-        <div className="mt-3 flex items-center justify-center">
-         {products?.length > 0 &&   <Products products={products}/>}
-        </div>
-        <div className="mt-3">
-            <Footer />
-        </div>
+      <div className="sticky top-3 z-10">
+        <NavbarWithMegaMenu query={query} setQuery={setQuery} />
+      </div>
+      <div className="mt-3">
+        <Slider />
+      </div>
+      <div className="mt-3 flex items-center justify-center">
+        <h3 className="uppercase font-sans text-xl font-bold">Products</h3>
+      </div>
+      <div className="mt-3 flex items-center justify-center">
+        <h4 className="bg-red-50 rounded">
+          Category:
+          <span className="pl-1 capitalize font-sans font-sm font-bold text-cyan-800">
+            {products?.[1]?.category ===
+            products?.[products.length - 1]?.category
+              ? products?.[1]?.category
+              : "General"}
+          </span>
+        </h4>
+      </div>
+      <div className="mt-3 flex items-center justify-center">
+        {products?.length > 0 && <Products products={products} />}
+      </div>
+      <div className="mt-3">
+        <Footer />
+      </div>
     </div>
   );
 }
