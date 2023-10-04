e2c696f1daf686493f79d9b1ebaf6d0eb0ed21cd
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-10-04 18:34:53 +0300

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 function Footer(){
     return(
-        <footer className="bg-white dark:bg-gray-900">
+        <footer className="border-2 border-gray-300 border-solid mb-2 rounded">
             <div className="mx-auto w-full max-w-screen-xl">
-                <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+                <div className="grid grid-cols-3 gap-10 px-10 py-10 lg:py-8 md:grid-cols-3">
                     <div>
                         <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
                         <ul className="text-gray-500 dark:text-gray-400 font-medium">
@@ -51,25 +51,8 @@ function Footer(){
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                            <li className="mb-4">
-                                <a href="#" className="hover:underline">iOS</a>
-                            </li>
-                            <li className="mb-4">
-                                <a href="#" className="hover:underline">Android</a>
-                            </li>
-                            <li className="mb-4">
-                                <a href="#" className="hover:underline">Windows</a>
-                            </li>
-                            <li className="mb-4">
-                                <a href="#" className="hover:underline">MacOS</a>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
-                <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+                <div className="px-4 py-6 md:flex md:items-center md:justify-between">
                     <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="#">Shire™</a>. All Rights Reserved.
                     </span>
                         <div className="flex mt-4 space-x-5 sm:justify-center md:mt-0">
